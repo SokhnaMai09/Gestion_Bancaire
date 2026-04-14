13bba73d9543993a8b4e7202dcb49f697595591f
SokhnaMai09 <maimounasene162@gmail.com>
2026-04-14 17:42:01 +0100

    Résolution du conflit DatabaseConnection

--- src/database/DatabaseConnection.java
+++ src/database/DatabaseConnection.java
@@ -5,20 +5,17 @@ import java.sql.DriverManager;
 
 public class DatabaseConnection {
 
-    private static final String URL =
-            "jdbc:mysql://localhost:3306/gestion_bancaire";
+    private static final String URL = "jdbc:mysql://localhost:3306/gestion_bancaire";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
     public static Connection getConnection() {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)
-            ;
-            
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Connexion réussie !");
             return conn;
         } catch (Exception e) {
+            System.out.println("Erreur de connexion !");
             e.printStackTrace();
             return null;
         }
